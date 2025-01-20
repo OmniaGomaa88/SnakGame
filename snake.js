1d901544b7de194792ca668034f267fcd19c1eb5
OmniaGomaa88 <minna1172018@gmail.com>
2025-01-20 09:29:06 +0100

    git inputs from keyEvent

--- snake.js
+++ snake.js
@@ -1,19 +1,18 @@
+import {getInputDirection } from "./input.js"
 export const snakSpeed = 1;
  //veriable as arry to make the snake as points x and y in the gridbord
 const snakeBody =[
-    {x:10, y:11},
-    {x:11, y:11},
-    {x:12, y:11},
-    {x:13, y:11},
-    {x:14, y:11}
+    {x:10, y:11}
+    
 ];
 
 export function update(){
+    let inputDirection = getInputDirection();
  for (let i = snakeBody.length -2 ; i>=0;i--){
     snakeBody[i+1]={ ...snakeBody[i]};
  }
- snakeBody[0].x +=0;
- snakeBody[0].y +=1;
+ snakeBody[0].x +=inputDirection.x;
+ snakeBody[0].y +=inputDirection.y;
  }
  
 export function drow(gameBord){
